a34be15c2235ec6214920c4adf656da22e29b46b
lenardjee <lenardvdmaas@hotmail.nl>
2017-04-07 10:53:11 +0200

    Update main.java

--- main.java
+++ main.java
@@ -2,8 +2,14 @@ package PWS;
 
 public class main extends JFrame {
 	
-	public static void main(String[] args) {
+	public static int width = Toolkit.getDefaultToolkit.getScreenWidth, height = Toolkit.getDefaultToolkit.getScreenHeight;
+	
+	public main() {
 		
+	}
+	
+	public static void main(String[] args) {
+		this.setContentPane(new contentPane(width, heigth, new Float(0.7)));
 	}
 	
 	
